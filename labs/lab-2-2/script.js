163cab99090350781dbd4dcb6f5e75a869ba0cfa
bad4iz <bad4iz@gmail.com>
2017-02-07 19:36:04 +0400

    promese ajax

--- labs/lab-2-2/script.js
+++ labs/lab-2-2/script.js
@@ -8,12 +8,11 @@ function getData(idBook) {
     return new Promise(function(resolve, reject){
         let xhr = new XMLHttpRequest();
         xhr.open("POST", 'postbooksbycat.php', true);
-        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
         xhr.onload = function () {
             if (xhr.status === 200){
                 let json = String(xhr.responseText);
-                console.log(json);
                 resolve(json);
             }else {
                 reject(xhr.status);
@@ -29,7 +28,7 @@ function getData(idBook) {
 
 
 getData(1)
-    .then(function(value){console.log(value)})
-    .catch(console.log("sdfsdfsafd"));
+    .then(function(value){console.log(value)});
+
 
 
