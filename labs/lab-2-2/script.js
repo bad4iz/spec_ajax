1fd328b9c90bd50d33bf2b0c60efa78002a80901
bad4iz <bad4iz@gmail.com>
2017-02-07 19:11:57 +0400

    promise

--- labs/lab-2-2/script.js
+++ labs/lab-2-2/script.js
@@ -1,14 +1,24 @@
 /**
  * Created by bad4iz on 07.02.2017.
  */
+
+
+function getData() {
+    
+}
 var xhr = new XMLHttpRequest();
 
 var body = 'name=' + encodeURIComponent(name) +
     '&surname=' + encodeURIComponent(surname);
 
-xhr.open("POST", '/submit', true)
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+xhr.open("POST", 'postbooksbycat.php', true);
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 xhr.onreadystatechange = ...;
 
 xhr.send(body);
+
+
+
+
+
