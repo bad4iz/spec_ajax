9dd8cb0117e90395d3a525473724b96ceb0d0ccc
bad4iz <bad4iz@gmail.com>
2017-02-07 20:21:38 +0400

    promese setTimeout

--- labs/lab-2-2/script.js
+++ labs/lab-2-2/script.js
@@ -33,8 +33,8 @@ function getData(idBook) {
 let timer = function() {
    return new Promise(function (resolve, reject) {
         setTimeout(() => {
-            Math.random() > .2 ? resolve("Все окей") : reject("не окей")
-        }, 3000
+            Math.random() > .5 ? resolve("Все окей") : reject("не окей")
+        }, 1000
         );
     });
 };
